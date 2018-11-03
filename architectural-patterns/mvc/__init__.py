85bbd451ba8c83de226634d6b2ef2eccf57258c0
HassanAzam <hassanazam55@gmail.com>
2018-11-03 21:48:55 +0500

    implemented MVC sample code

---
+++ architectural-patterns/mvc/__init__.py
@@ -0,0 +1,5 @@
+from controller import start
+
+if __name__ == "__main__":
+   #running controller function
+   start()
---
+++ architectural-patterns/mvc/controller.py
@@ -0,0 +1,16 @@
+from model import Person
+import view
+
+def showAll():
+   #gets list of all Person objects
+   people_in_db = Person.getAll()
+   #calls view
+   return view.showAllView(people_in_db)
+
+def start():
+   view.startView()
+   user_input = input()
+   if input == 'y':
+      return showAll()
+   else:
+      return view.endView()
